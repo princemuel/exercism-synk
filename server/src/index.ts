a49ed3de55fb4fac6744649d9cd6300d5d56e526
princemuel <vansomecsam@gmail.com>
2022-08-22 03:57:44 +0100

    added dummy data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello Wold on the command line')
+import dotenv from 'dotenv-safe';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { schema } from './schema';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    // @ts-expect-error: the library definition is wrong
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
