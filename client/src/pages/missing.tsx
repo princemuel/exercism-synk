cd0429495a4cb38d052f3dc87c8a8c345cfe9e42
princemuel <vansomecsam@gmail.com>
2022-08-23 18:09:27 +0100

    add missing route

--- client/src/pages/missing.tsx
+++ client/src/pages/missing.tsx
@@ -1,4 +1,5 @@
 import { FaExclamationTriangle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 type Props = {};
 
@@ -6,6 +7,11 @@ const Missing = (props: Props) => {
   return (
     <div className='d-flex flex-column justify-content-conter align-items-center mt-5'>
       <FaExclamationTriangle className='text-danger' size={'3em'} />
+      <h1>404</h1>
+      <p className='lead'>Sorry, this page does not exist</p>
+      <Link to='/' className='btn btn-primary'>
+        Go To Home
+      </Link>
     </div>
   );
 };
