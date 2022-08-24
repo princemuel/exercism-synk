001189b971b66a29ed12cf9d26b143a03049175f
princemuel <vansomecsam@gmail.com>
2022-08-24 01:48:40 +0100

    gql finished

--- client/src/components/client-row.tsx
+++ client/src/components/client-row.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from '@apollo/client';
 import { FaTrash } from 'react-icons/fa';
 import { DELETE_CLIENT } from '../mutations';
-import { GET_CLIENTS } from '../queries';
+import { GET_CLIENTS, GET_PROJECTS } from '../queries';
 import { IClient } from '../types';
 
 type Props = {
@@ -12,21 +12,22 @@ type Props = {
 const ClientRow = ({ client }: Props) => {
   const [deleteClient] = useMutation<IClient>(DELETE_CLIENT, {
     variables: { id: client.id },
-    //  refetchQueries: [{ query: GET_CLIENTS },],
-    //@ts-expect-error
-    update(cache, { data: { deleteClient } }) {
-      //@ts-expect-error
-      const { clients } = cache.readQuery({
-        query: GET_CLIENTS,
-      });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          //@ts-expect-error
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    // wipes out the client and its projects and then updates the ui
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+
+    // update(cache, { data: { deleteClient } }) {
+    //   //@ts-expect-error
+    //   const { clients } = cache.readQuery({
+    //     query: GET_CLIENTS,
+    //   });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       //@ts-expect-error
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
 
   return (
